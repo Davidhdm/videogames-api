0133d758a9b646b9eb7b6e5095b84d98853afc8c
Davidhdm <david.hdm9@gmail.com>
2021-09-28 17:31:24 +0200

    games route and GameCard component

---
+++ src/components/GameCard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="game_card">
+    GameCard component
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GameCard'
+}
+</script>
---
+++ src/views/Games.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="games">
+    <GameCard/>
+  </div>
+</template>
+
+<script>
+import GameCard from '@/components/GameCard.vue'
+
+export default {
+  name: 'Games',
+  components: {
+    GameCard
+  }
+}
+</script>
