4da267bcb471d4502718fe720373c551acada043
Davidhdm <david.hdm9@gmail.com>
2021-10-08 20:51:27 +0200

    deleted unncessary files

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,6 @@
 <template>
-  <!-- <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/games">Games</router-link>
-  </div>-->
   <router-view/>
 </template>
-
-<script>
-
-export default {
-  components: {
-    /* Games */
-  }
-}
-</script>
 
 <style>
 html {
@@ -23,9 +9,7 @@ html {
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-/*   -webkit-font-smoothing: antialiased;
+  -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center; */
-  color: #2c3e50;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,12 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-/* import Home from '../views/Home.vue' */
 import Games from '../views/Games.vue'
 
 const routes = [
-  /* {
-    path: '/',
-    name: 'Home',
-    component: Home
-  }, */
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
   {
     path: '/',
     name: 'Games',
     component: Games
+    /* component: () => import( */ /* webpackChunkName: "about" */ /* '../views/Games.vue') */
   }
 ]
 
